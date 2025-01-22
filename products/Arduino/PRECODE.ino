94056162405ecf531f8ec3d5e7fc1f7d32ee927a
Oskar Nilzen <oskar.nilzen@volvocars.com>
2025-01-22 18:32:06 +0100

    Added testfiles for comms between RPIE and Arduino

--- products/Arduino/PRECODE.ino
+++ products/Arduino/PRECODE.ino
@@ -1,21 +1,21 @@
 #include <Servo.h>
 
-
 Servo servo;
 int channel[4];
 
-int esc_pins[4] = {6,9,10,11};
+int esc_pins[4] = {6, 9, 10, 11};
 
 byte chanel[4];
 int throttle = 1000;
 
-void setup() {
-  
- Serial.begin(9600);
- servo.attach(9);
+void setup()
+{
+  Serial.begin(9600);
+  servo.attach(9);
 }
 
-void loop() {
+void loop()
+{
 
   throttle = analogRead(A0);
   throttle = map(throttle, 0, 255, 1000, 2000);
